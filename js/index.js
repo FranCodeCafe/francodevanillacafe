f982a446c72ebb0079c3ebd042c6ad44b4c5e746
FranCodeCafe <franciscasalinascastillo@gmail.com>
2020-04-16 23:29:24 -0400

    responsive navbar menu, it still has some details

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 function burger() {
-    var x = document.getElementById("navbar-menu");
-    if (x.className === "navbar-menu") {
-        x.className += " responsive";
-        } else {
-        x.className = "navbar-menu";
-        }
-}
+        var x = document.getElementById("navbar");
+        if (x.className === "navbar") {
+            x.className += " responsive";
+            } else {
+            x.className = "navbar";
+            }
+
+        var y = document.getElementById("navbar-menu");
+        if (y.className === "navbar-menu") {
+            y.className += " responsive";
+            } else {
+            y.className = "navbar-menu";
+            }
+    }
